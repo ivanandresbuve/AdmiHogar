2f120acd61defeaa4a035ac9b8dbec4d2f2d2563
ivanandresbuve <ivanandresbuve@unicauca.edu.co>
2020-05-25 23:15:34 -0500

    Se puso el icono en el action bar y se cambio su color

--- app/src/main/java/edu/unicauca/admihogar/MainActivity.java
+++ app/src/main/java/edu/unicauca/admihogar/MainActivity.java
@@ -10,8 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //tiempo para el splash_screen
         try {
             Thread.sleep(5000);
         }catch (Exception ex){}
+
+        //Poner el icono en el action Bar
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
     }
 }
